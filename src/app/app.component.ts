fa61222f6575af747bb3f36a4b3bbb9c749b69d3
csandrisTurr <csaszarandras@turr.hu>
2024-11-19 12:24:16 +0100

    feat: cuccok

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,26 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
 import { CommonModule } from '@angular/common';
+import { TableModule } from 'primeng/table';
+import { InputTextModule } from 'primeng/inputtext';
+import { FormsModule } from '@angular/forms';
+import { FloatLabelModule } from 'primeng/floatlabel';
+import { PopoverModule } from 'primeng/popover';
+import { ListboxModule } from 'primeng/listbox';
+import { SelectModule } from 'primeng/select';
+// import { Toast } from 'primeng/toast';
+
+enum TodoState {
+  Todo = 'todo',
+  Doing = 'doing',
+  Done = 'done',
+}
+
+interface TodoItem {
+  no: number;
+  state: TodoState;
+  name: string;
+}
 
 @Component({
   selector: 'app-root',
@@ -9,11 +29,53 @@ import { CommonModule } from '@angular/common';
   imports: [
     CommonModule,
     RouterOutlet,
-    ButtonModule
+    ButtonModule,
+    TableModule,
+    InputTextModule,
+    FormsModule,
+    FloatLabelModule,
+    PopoverModule,
+    ListboxModule,
+    SelectModule,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'todo';
+  newItemName?: string;
+  list: TodoItem[] = [];
+  todoStates = [
+    TodoState.Todo,
+    TodoState.Doing,
+    TodoState.Done,
+  ];
+  lastId = 0;
+
+  addItemButton() {
+    this.list.push({
+      no: this.lastId + 1,
+      state: TodoState.Todo,
+      name: this.newItemName!,
+    });
+
+    this.lastId++;
+  }
+
+  changeTodoState(no: number, event: any) {
+
+  }
+  
+  todoToIcon(state: TodoState) {
+    switch (state) {
+      case TodoState.Todo: return 'circle-off';
+      case TodoState.Doing: return 'circle-on';
+      case TodoState.Done: return 'check';
+    }
+  }
+
+  capitalize(str: string) {
+    return str.charAt(0).toUpperCase()
+      + str.slice(1);
+  }
 }
